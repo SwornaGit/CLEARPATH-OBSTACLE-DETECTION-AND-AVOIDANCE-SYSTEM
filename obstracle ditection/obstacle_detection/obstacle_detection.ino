78b4f4a8ca8d06c50aee194507b1d33c6ab1cd20
Sworna Roy <>
2025-12-29 15:42:26 +0100

    set up motor A and B

---
+++ obstracle ditection/obstacle_detection/obstacle_detection.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Motor A (Left Motor)
+#define ENA_PIN 13   // PWM
+#define IN1_PIN 12  // Forward
+#define IN2_PIN 11   // Backward
+
+
+// Motor B (Right Motor)
+#define ENB_PIN 9    // PWM
+#define IN3_PIN 10  // Forward
+#define IN4_PIN 8   // backward
+
